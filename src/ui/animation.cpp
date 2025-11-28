45be51bd83c22d289850935a303ec6a3dfc0c459
Kiry Ratanak <kiryratanak71@.com>
2025-11-28 16:48:13 +0700

    edit

--- src/ui/animation.cpp
+++ src/ui/animation.cpp
@@ -3,53 +3,46 @@
 #include <thread>
 #include <windows.h>
 
+using namespace std;
+
 // Some MinGW versions do NOT define this
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #endif
 
-void enableVTMode() {
+void enableVTMode()
+{
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD dwMode = 0;
     GetConsoleMode(hOut, &dwMode);
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(hOut, dwMode);
-}   
-
-void blockMove() {
-    for (int i = 20 ; i = 0; i++) {
-        std::cout << "\rMoving: [" << std::string(i, '#') << "]";
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    std::cout << std::endl;
 }
 
-void blockUnblock() {
-    for (int i = 0; i >= 0; i--) {
-        std::cout << "\rUnblocking: [" << std::string(i, '#') << "]";
-        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+void fillAndEmpty()
+{
+    string red = "\033[31m";
+    string green = "\033[32m";
+    string reset = "\033[0m";
+    cout << "[\r\033[62C" ;
+    for (int i = 0; i <= 30; i++)
+    {
+        cout << "\r\033[63C" << red << string(i, '-') << "/" << reset;
+        this_thread::sleep_for(chrono::milliseconds(80));
     }
-    std::cout << std::endl;
+    cout << "]";
+    for (int i = 0; i <= 30; i++)
+    {
+        cout << "\r\033[63C" << green << string(i, '#') << green << "/" << reset;
+        this_thread::sleep_for(chrono::milliseconds(80));
+    }
+
+    cout << "]" <<endl;
+
 }
 
-void fillAndEmpty() {
-    std::string purple = "\033[35m";
-    std::string reset  = "\033[0m";
-
-    for (int i = 1; i <= 20; i++) {
-        std::cout << "\r[" << purple << std::string(i, '#') << reset << "]";
-        std::this_thread::sleep_for(std::chrono::milliseconds(80));
-    }
-
-    for (int i = 20; i >= 1; i--) {
-        std::cout << "\r[" << purple << std::string(i, '#') << reset << "]";
-        std::this_thread::sleep_for(std::chrono::milliseconds(80));
-    }
-
-    std::cout << std::endl;
-}
-
-int main() {
+int main()
+{
     enableVTMode();
     fillAndEmpty();
     return 0;
